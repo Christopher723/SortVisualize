3d9d11afb67af2d220c6ca07f7c5d09e5016c1b6
Christopher <Christopher.woods723@gmail.com>
2023-10-10 08:18:15 -0400

    Changed bubble sort algorithm to work and update view

--- SortVisualize/bubbleSort.swift
+++ SortVisualize/bubbleSort.swift
@@ -16,26 +16,29 @@ struct bubbleSort: View {
         Text("Bubble")
     }
     
-    func bubbleSort(){
+    func bubbleSort() {
         isSorting = true
-        while isSorting{
-            for _ in 0..<14{
-                for j in 0..<14{
-                    if randomArray[j] > randomArray[j + 1]{
-                        let tempNum = randomArray[j]
-                        randomArray[j] = randomArray[j + 1]
-                        randomArray[j+1] = tempNum
-                        
-                    }
-                }
-                
-            }
-            isSorting = false
-        }
         
-    
+        DispatchQueue.global(qos: .userInteractive).async {
+            var swapped = false
+            repeat {
+                swapped = false
+                for j in 0..<(self.randomArray.count - 1) {
+                    if self.randomArray[j] > self.randomArray[j + 1] {
+                        DispatchQueue.main.async {
+                            self.randomArray.swapAt(j, j + 1)
+                        }
+                        swapped = true
+                    }
+                    // Introduce a delay here
+                    Thread.sleep(forTimeInterval: 0.05) // Adjust the delay time as needed
+                }
+            } while swapped
+            
+            // When sorting is complete, update the UI on the main thread
+            DispatchQueue.main.async {
+                self.isSorting = false
+            }
+        }
     }
-    
 }
-
-
